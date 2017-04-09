c99f18650474a6f4426ce9a36094c65e41a3ca1e
niiyz <yoshida@niiyz.com>
2017-04-09 17:29:43 +0900

    $pref -> $pref_code

--- geojson_to_topojson.sh
+++ geojson_to_topojson.sh
@@ -3,14 +3,14 @@
 find ./geojson -type f | while read file
 do
     arr=( `echo $file | tr -s '/' ' '`)
-    pref=${arr[2]}
+    pref_code=${arr[2]}
     # ディレクトリ
-    dir="topojson/"$pref
+    dir="topojson/"$pref_code
     if [ ! -d $dir ]; then
       mkdir -p $dir
     fi
     basename=$(basename $file)
     name=( `echo $basename | sed -e "s/\.json/.topojson/"`)
     echo $name
-    $(npm bin)/geo2topo -o $dir/$name -q 1e4 geojson/$pref/$basename
+    $(npm bin)/geo2topo -o $dir/$name -q 1e4 geojson/$pref_code/$basename
 done
