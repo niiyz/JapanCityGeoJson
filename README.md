3764368d67d2491bbca840cc7061263ac19ed125
Tetsuya Yoshida <yoshida@niiyz.com>
2020-12-17 11:04:28 +0900

    Fix

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 47都道府県の県・市・町・村・郡・区の形を作るための[GeoJsonデータ](/geojson)、[TopoJsonデータ](/topojson)です。
 
-
 国土数値情報 (JPGIS2.1(GML)準拠及びSHAPE形式データ)　国土交通省
-
-国土交通省国土政策局GISHP http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html
+https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v2_4.html#prefecture00
 
 GeoJson http://geojson.org/
 
 TopoJson https://github.com/mbostock/topojson
 
+```
+docker-compose up
 
+docker-compose exec app unzip N03-20200101_GML.zip N03-20_200101.geojson
 
+docker-compose exec app go run main.go
+```
--- main.go
+++ main.go
@@ -1,9 +1,101 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"./geo"
+	"io/ioutil"
+	"os"
 )
 
-func main(){
-	log.Println("JapanCityGeoJson")
+// City
+func makeCityGeoJson(raw []byte) {
+	// Split Data
+	cityMap := geo.Split(geo.SPLIT_TYPE_CITY, raw)
+
+	// Loop
+	for city, fts := range cityMap {
+		// Pref Name
+		pref := fts[0].GetPref()
+		fmt.Println(pref, city, cap(fts))
+		// Save Dir
+		dir := "geojson/" + pref
+		if !isExist(dir) {
+			os.MkdirAll(dir, 0777)
+		}
+		// Save Path
+		path := dir + "/" + city + ".json"
+		// Save Json
+		geo.Save(path, fts)
+	}
+}
+
+// County
+func makeCountyGeoJson(raw []byte) {
+	// Split Data
+	cityMap := geo.Split(geo.SPLIT_TYPE_COUNTY, raw)
+
+	// Loop
+	for county, fts := range cityMap {
+		// Pref Name
+		pref := fts[0].GetPref()
+		fmt.Println(county, cap(fts))
+		// Save Dir
+		dir := "geojson/" + pref
+		if !isExist(dir) {
+			os.MkdirAll(dir, 0777)
+		}
+		// Save Path
+		path := dir + "/" + county + ".json"
+		// Save Json
+		geo.Save(path, fts)
+	}
+}
+
+// Pref
+func makePrefGeoJson(raw []byte) {
+	// Split Data
+	cityMap := geo.Split(geo.SPLIT_TYPE_PREF, raw)
+	// Loop
+	for pref, fts := range cityMap {
+		fmt.Println(pref, cap(fts))
+		// Save Dir
+		dir := "geojson/47都道府県"
+		if !isExist(dir) {
+			os.MkdirAll(dir, 0777)
+		}
+		// Save Path
+		path := dir + "/" + pref + ".json"
+		// Save Json
+		geo.Save(path, fts)
+	}
+}
+
+func reset() {
+	os.RemoveAll("geojson")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Not Exists GeoJsonPath")
+		os.Exit(1)
+	}
+	geoJsonFilePath := os.Args[1]
+	raw, err := ioutil.ReadFile(geoJsonFilePath)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	reset()
+	// City
+	makeCityGeoJson(raw)
+	// Pref
+	// makePrefGeoJson(raw)
+	// County
+	// makeCountyGeoJson(raw)
+}
+
+// Check Dir Exist
+func isExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
