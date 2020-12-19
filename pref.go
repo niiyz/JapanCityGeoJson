04f09b3006858f6ffa02dfc3443453e04b35bc98
Tetsuya Yoshida <yoshida@niiyz.com>
2020-12-19 09:28:34 +0900

    Add Pref

---
+++ pref.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+var (
+	pref = [47]string{
+		"北海道",
+		"青森県",
+		"岩手県",
+		"宮城県",
+		"秋田県",
+		"山形県",
+		"福島県",
+		"茨城県",
+		"栃木県",
+		"群馬県",
+		"埼玉県",
+		"千葉県",
+		"東京都",
+		"神奈川県",
+		"新潟県",
+		"富山県",
+		"石川県",
+		"福井県",
+		"山梨県",
+		"長野県",
+		"岐阜県",
+		"静岡県",
+		"愛知県",
+		"三重県",
+		"滋賀県",
+		"京都府",
+		"大阪府",
+		"兵庫県",
+		"奈良県",
+		"和歌山県",
+		"鳥取県",
+		"島根県",
+		"岡山県",
+		"広島県",
+		"山口県",
+		"徳島県",
+		"香川県",
+		"愛媛県",
+		"高知県",
+		"福岡県",
+		"佐賀県",
+		"長崎県",
+		"熊本県",
+		"大分県",
+		"宮崎県",
+		"鹿児島県",
+		"沖縄県",
+	}
+)
+
+func indexOf(element string, data [47]string) int {
+	for index, v := range data {
+		if element == v {
+			return index
+		}
+	}
+	return -1
+}
+
+func GetPrefCode(prefName string) string {
+	if prefIndex := indexOf(prefName, pref); prefIndex >= 0 {
+		return fmt.Sprintf("%2d", prefIndex + 1)
+	}
+	return ""
+}
