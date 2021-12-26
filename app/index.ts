66fcce32b1225d655a98b1d05c1eca7cd8bf3f57
Tetsuya Yoshida <yoshida@niiyz.com>
2021-12-26 18:17:20 +0900

    updated

--- app/index.ts
+++ app/index.ts
@@ -15,29 +15,33 @@ const pgConnect = async (): Promise<Client> => {
 const main = async (): Promise<void> => {
 
     const client = await pgConnect();
-    const res = await client.query('SELECT json_build_object(\n' +
-        '    \'type\', \'FeatureCollection\',\n' +
-        '    \'features\', json_agg(ST_AsGeoJSON(pref.*)::json)\n' +
-        '    )\n' +
-        'FROM (\n' +
-        '\tselect\n' +
-        '\t\tcode as id, name, (ST_Dump(ST_Union(geom))).geom\n' +
-        '\tfrom\n' +
-        '\t\tprefectures\n' +
-        '\twhere\n' +
-        '\t\tcode = \'01\'\n' +
-        '\tgroup by\n' +
-        '\t\tcode, name\n' +
-        ') as pref;');
+    const sql = `
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'features', json_agg(ST_AsGeoJSON(pref.*)::json)
+    )
+FROM (
+	select
+		code as id, name, ST_Union(geom)
+	from
+		prefectures
+	where
+	    code = $1
+	group by
+		code, name
+) as pref;
+`;
+    const res = await client.query('select code, name from prefectures group by code, name order by code');
 
     const fs = require("fs");
 
-    try {
-        fs.writeFileSync("test.json", JSON.stringify(res.rows[0].json_build_object));
-    } catch(e: any){
-        console.log(e.message);
+    for (let i in res.rows) {
+        const code = res.rows[i].code;
+        const name = res.rows[i].name;
+        console.log(code, name);
+        const json = await client.query(sql, [code]);
+        fs.writeFileSync(`${code}.json`, JSON.stringify(json.rows[0].json_build_object));
     }
-
     await client.end()
 }
 
